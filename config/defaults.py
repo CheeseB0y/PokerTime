f716102eafe88555da769ccad1a882a2709e132d
CheeseB0y <cheeseb0y1316@gmail.com>
2026-01-16 20:18:49 -0500

    Added a new setting for screen flash duration

--- config/defaults.py
+++ config/defaults.py
@@ -2,5 +2,6 @@ DEFAULT_SETTINGS = {
     "play_alarm_sound": True,
     "alarm_volume": 0.5,
     "flash_screen": True,
+    "flash_duration": 5,
     "auto_start_next_round": True,
 }
--- models/settings.py
+++ models/settings.py
@@ -3,6 +3,7 @@ class Settings:
         self.play_alarm_sound = config["play_alarm_sound"]
         self.alarm_volume = config["alarm_volume"]
         self.flash_screen = config["flash_screen"]
+        self.flash_duration = config["flash_duration"]
         self.auto_start_next_round = config["auto_start_next_round"]
 
     def to_dict(self) -> dict:
@@ -10,5 +11,6 @@ class Settings:
             "play_alarm_sound": self.play_alarm_sound,
             "alarm_volume": self.alarm_volume,
             "flash_screen": self.flash_screen,
+            "flash_duration": self.flash_duration,
             "auto_start_next_round": self.auto_start_next_round,
         }
