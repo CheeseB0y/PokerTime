814f77660a25b06f2732c42d6182204451e84d5f
CheeseB0y <cheeseb0y1316@gmail.com>
2026-01-23 23:20:55 -0500

    Settings page is now functional

--- gui/settings_page.py
+++ gui/settings_page.py
@@ -1,6 +1,7 @@
 import tkinter as tk
+from tkinter import ttk
 from tktooltip import ToolTip
-from gui.theme import BG_COLOR
+from config.manager import save_settings
 
 
 class SettingsPage:
@@ -13,15 +14,66 @@ class SettingsPage:
         window = tk.Toplevel(ctx.root)
         window.title("Settings")
         window.geometry("800x600")
-        window.configure(bg=BG_COLOR)
+        window.configure(bg=self.ctx.bg_color)
 
-        sound_checkbox = tk.Checkbutton(window, text="Play sound on round finish")
-        sound_checkbox.pack()
-        flash_checkbox = tk.Checkbutton(window, text="Flash screen on round finish")
-        flash_checkbox.pack()
-        automatic_round_start_checkbox = tk.Checkbutton(
-            window, text="Start next round automatically"
+        self.play_alarm_sound_var = tk.BooleanVar(
+            value=self.ctx.settings.play_alarm_sound
         )
-        automatic_round_start_checkbox.pack()
+        self.play_alarm_sound_checkbox = ttk.Checkbutton(
+            window,
+            text="Play sound on round finish",
+            variable=self.play_alarm_sound_var,
+        )
+        self.play_alarm_sound_checkbox.pack()
+        self.flash_screen_var = tk.BooleanVar(value=self.ctx.settings.flash_screen)
+        self.flash_screen_checkbox = ttk.Checkbutton(
+            window, text="Flash screen on round finish", variable=self.flash_screen_var
+        )
+        self.flash_screen_checkbox.pack()
+        self.auto_start_next_round_var = tk.BooleanVar(
+            value=self.ctx.settings.auto_start_next_round
+        )
+        self.auto_start_next_round_checkbox = ttk.Checkbutton(
+            window,
+            text="Start next round automatically",
+            variable=self.auto_start_next_round_var,
+        )
+        self.auto_start_next_round_checkbox.pack()
+        self.use_24_hour_clock_var = tk.BooleanVar(
+            value=self.ctx.settings.use_24_hour_clock
+        )
+        self.use_24_hour_clock_checkbox = ttk.Checkbutton(
+            window,
+            text="24 hour clock",
+            variable=self.use_24_hour_clock_var,
+        )
+        self.use_24_hour_clock_checkbox.pack()
 
-        ToolTip(sound_checkbox, msg="test tip")
+        ToolTip(self.play_alarm_sound_checkbox, msg="test tip")
+
+        save_changes_button = tk.Button(
+            window,
+            text="Save Changes",
+            command=self.save_changes,
+        )
+        save_changes_button.pack()
+
+        apply_changes_button = tk.Button(
+            window,
+            text="Apply Changes",
+            command=self.apply_changes,
+        )
+        apply_changes_button.pack()
+
+    def save_changes(self):
+        self.update_settings()
+        save_settings(settings=self.ctx.settings)
+
+    def apply_changes(self):
+        self.update_settings()
+
+    def update_settings(self):
+        self.ctx.settings.play_alarm_sound = self.play_alarm_sound_var.get()
+        self.ctx.settings.flash_screen = self.flash_screen_var.get()
+        self.ctx.settings.auto_start_next_round = self.auto_start_next_round_var.get()
+        self.ctx.settings.use_24_hour_clock = self.use_24_hour_clock_var.get()
